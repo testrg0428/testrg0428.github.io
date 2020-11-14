34f99b99b64463533b7e93daa5d20c54421b928f
testrg0428 <testrg@yahoo.com.tw>
2020-11-14 21:33:56 +0800

    20201114

--- upload.sh
+++ upload.sh
@@ -7,8 +7,8 @@
 
 #git add -A
 git add .
-#git checkout -b gh-pages
+git checkout -b gh-pages
 #git checkout -b main
 git commit -m `date +%Y%m%d`
-git push origin main
-#git push --set-upstream origin gh-pages
+#git push origin main
+git push --set-upstream origin gh-pages
